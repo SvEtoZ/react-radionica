16558b5e6c30d65efcd9db36690ae41803eb081e
Nikola <perisicnikola37@gmail.com>
2024-04-12 10:59:40 +0200

    Finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react';
+import './App.css';
+import sayHi from './files/module'; // Importing default export
+import { name, age } from './files/module'; // Importing named exports
+
+// import sayHi, { name, age } from './files/module'; 
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    sayHi();
+  }, []);
+
+  let x = 10
+  x = 20 // moguće
+
+  const y = 5
+  // y = 8 nije moguće
+
+  useEffect(() => {
+    if (true) {
+      let z = 30;
+      console.log(z); // output: 30
+    }
+    // console.log(z); // greška, varijabla "z" nije definisana izvan bloka if
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h3>Named vs default exports</h3>
+      <p>Name: {name}</p>
+      <p>Age: {age}</p>
     </>
   )
 }
 
-export default App
+export default App;
